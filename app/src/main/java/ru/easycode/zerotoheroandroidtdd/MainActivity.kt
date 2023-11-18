fb5255d3c969e2de2f928df99c2fe061ee166329
MaximVideshyn <68384889+MaximVideshyn@users.noreply.github.com>
2023-11-18 21:26:59 +0100

    start with Ui test

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,17 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pb = findViewById<ProgressBar>(R.id.progressBar)
+        val btn = findViewById<Button>(R.id.actionButton)
+
+
     }
 }
