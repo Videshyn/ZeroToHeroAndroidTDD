56149d4fca830f3e84556ee8024598810228426b
MaximVideshyn <68384889+MaximVideshyn@users.noreply.github.com>
2023-11-18 20:03:31 +0100

    cherry pick changes from test 6

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,17 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv = findViewById<TextView>(R.id.titleTextView)
+        findViewById<Button>(R.id.changeButton).setOnClickListener {
+            tv.setText(R.string.i_am_android_developer)
+        }
     }
 }
