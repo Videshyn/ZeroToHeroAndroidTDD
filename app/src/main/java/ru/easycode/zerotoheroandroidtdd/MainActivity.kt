412bfb8a18e651eb8793d621db00b1a89f4d42d7
MaximVideshyn <68384889+MaximVideshyn@users.noreply.github.com>
2023-11-18 20:22:08 +0100

    finished test 9

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,23 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv = findViewById<TextView>(R.id.titleTextView)
+        val btn = findViewById<Button>(R.id.removeButton)
+        val root = findViewById<LinearLayout>(R.id.rootLayout)
+
+        if (savedInstanceState != null) {
+            root.removeView(tv)
+        }
+
+        btn.setOnClickListener { root.removeView(tv) }
     }
 }
