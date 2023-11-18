05cde394a7576e292313604764e3f7b14eebf6c6
MaximVideshyn <68384889+MaximVideshyn@users.noreply.github.com>
2023-11-18 20:40:25 +0100

    finished test 10

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,44 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.removeButton)
+        val tv = findViewById<TextView>(R.id.titleTextView)
+        val root = findViewById<LinearLayout>(R.id.rootLayout)
+
+        savedInstanceState?.let {
+            if (it.getBoolean(KEY, false)) {
+                removeViewAndDisableButton(btn, root, tv)
+            }
+        }
+
+        btn.setOnClickListener {
+            removeViewAndDisableButton(btn, root, tv)
+        }
+    }
+
+    private fun removeViewAndDisableButton(btn: Button, root: LinearLayout, tv: TextView) {
+        btn.isEnabled = false
+        root.removeView(tv)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY, !btn.isEnabled)
+    }
+
+    companion object {
+        private const val KEY = "SHOULD_DISABLE"
     }
 }
